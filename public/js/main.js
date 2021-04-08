1ebdd1742b292eeac7e817146c2a03c57d31fc12
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2021-04-08 16:06:31 -0400

    fighting with the environment

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,12 @@
 "use strict";
+//const dotenv = require("dotenv").config();
+//import dotenv from "dotenv";
+//import config from "./config/index.ts";
 
 document.addEventListener("DOMContentLoaded", function(event) {
+	//dotenv.config();
 	console.log("Ready!");
+	//console.log(process.env.API_KEY);
+	console.log(process.env.API_KEY);
+
 });
